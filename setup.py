2545efd5f805b12aaf694cd21a390a2d607979ee
jzlfirst@foxmail.com <dtid_5442f0409e8b3430>
2018-11-12 03:38:12 +0000

    easy setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "scangates",
+    version = "0.1.0",
+    description = "scan gates virus.",
+    license = "MIT License",
+    url = "https://github.com/ZheMoon/scangates",
+    author = "ZheMoon",
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = required
+)
